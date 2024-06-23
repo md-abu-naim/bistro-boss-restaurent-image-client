430a5d8a533c51ac4948db21542ef795cf4d98e2
abunaim34 <farhanadnanfarabi@gmail.com>
2024-06-23 22:18:45 +0600

    first commit

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle  heading='payment' subHeading='Please Pay to eat' />
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
